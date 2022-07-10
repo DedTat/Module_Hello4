b12b75c0195c958de11135fe4ef50414d45bce0d
DedTat <61464184+DedTat@users.noreply.github.com>
2022-07-10 21:20:44 +0300

    Initial commit

---
+++ hello.c
@@ -0,0 +1,33 @@
+/*
+ * hello_4.c - Демонстрация описания модуля.
+ */
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#define DRIVER_AUTHOR "Peter Jay Salzman <p@dirac.org>"
+#define DRIVER_DESC "A sample driver"
+
+static int __init init_hello_4(void) {
+  printk(KERN_ALERT "Hello, world 4\n");
+  return 0;
+}
+
+static void __exit cleanup_hello_4(void) {
+  printk(KERN_ALERT "Goodbye, world 4\n");
+}
+
+module_init(init_hello_4);
+module_exit(cleanup_hello_4);
+
+/*
+ * Вы можете передавать в макросы строки, как это показано ниже:
+ * Запретить вывод предупреждения о "загрязнении" ядра, объявив код под GPL.
+ */
+MODULE_LICENSE("GPL");
+
+/*
+ * или определения:
+ */
+MODULE_AUTHOR(DRIVER_AUTHOR); /* Автор модуля */
+MODULE_DESCRIPTION(DRIVER_DESC); /* Назначение модуля */
+MODULE_SUPPORTED_DEVICE("testdevice");
